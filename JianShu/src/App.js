ea1458ff6ec43d5d5f8d07f60b5b1d10a6a83d0c
jsliang <1741020489@qq.com>
2019-04-07 20:46:08 +0800

    React 系列 - Demo Two - 简书

--- JianShu/src/App.js
+++ JianShu/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './common/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello React!
+        <Header />
       </div>
     );
   }
--- JianShu/src/index.js
+++ JianShu/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './reset.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
