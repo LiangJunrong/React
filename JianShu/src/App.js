733af637f2ebdfc1de2bf2326086aa5ffde74aed
jsliang <1741020489@qq.com>
2019-04-14 17:44:16 +0800

    React 系列 - Demo Three - 简书

--- JianShu/src/App.js
+++ JianShu/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Header from './common/header';
 import store from './store';
+// 1. 引入 React 路由的 BrowserRouter 和 Route
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store} className="App">
         <Header />
+        {/* 2. 在页面中使用 React 路由 */}
+        <BrowserRouter>
+          <Route path="/" exact render={() => <div>HOME</div>}></Route>
+          <Route path="/detail" exact render={() => <div>DETAIL</div>}></Route>
+        </BrowserRouter>
       </Provider>
     );
   }
