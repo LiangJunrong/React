be9f4881a34a76e1df84b5773f2143a6ac705624
jsliang <1741020489@qq.com>
2019-04-16 08:25:51 +0800

    React 系列 - Demo Three - 简书

--- JianShu/src/pages/home/components/LeftList.js
+++ JianShu/src/pages/home/components/LeftList.js
@@ -1,10 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class LeftList extends Component {
   render() {
     return (
       <div className="left-list">
-        左侧
+        <div className="left-list-top">
+          <ul className="left-list-top-left">
+            <li>
+              <Link to='remen'>热门</Link>
+            </li>
+            |
+            <li>
+              <Link to='zuixin'>最新</Link>
+            </li>
+            |
+            <li>
+              <Link to='pinglun'>评论</Link>
+            </li>
+          </ul>
+          <ul className="left-list-top-right">
+            <li>
+              <Link to='benzhouzuire'>本周最热</Link>
+            </li>
+            ·
+            <li>
+              <Link to='benyuezuire'>本月最热</Link>
+            </li>
+            ·
+            <li>
+              <Link to='lishizuire'>历史最热</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="left-list-container">
+          
+        </div>
       </div>
     )
   }
