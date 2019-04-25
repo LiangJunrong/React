ebf6286992841386dd97f6c8d3154ec4857cdeed
jsliang <1741020489@qq.com>
2019-04-25 22:43:05 +0800

    React 系列 - Demo Four - 掘金

--- JueJin/src/App.js
+++ JueJin/src/App.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Fragment>
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={Home}></Route>
+      </BrowserRouter>
     </Fragment>
   );
 }
--- JueJin/src/components/Header/index.js
+++ JueJin/src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import './index.css';
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <h1>Header</h1>
+        <div className="header-container">
+          <Link to="home">首页</Link>
+        </div>
       </header>
     )
   }
