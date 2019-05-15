df309fc3a71150b0738ea9064ce4293d7e68b4ee
梁峻荣 <liangjunrong@shein.com>
2019-05-15 16:08:55 +0800

    React 系列 - Demo Four - 掘金

--- JueJin/src/App.js
+++ JueJin/src/App.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header';
 import TimeLine from './pages/TimeLine';
 import NotFound from './pages/404';
@@ -10,13 +9,11 @@ function App() {
     <Fragment>
       <BrowserRouter>
         <Header />
-        <ScrollToTop>
-          <Switch>
-            <Redirect from="/" to="/timeline/recommend" exact />
-            <Route path="/timeline/recommend" component={TimeLine}></Route>
-            <Route component={NotFound}></Route>
-          </Switch>
-        </ScrollToTop>
+        <Switch>
+          <Redirect from="/" to="/timeline/recommend" exact />
+          <Route path="/timeline/recommend" component={TimeLine}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
       </BrowserRouter>
     </Fragment>
   );
